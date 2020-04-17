3642d44e4e8586a91c0756b4f799c46f621f9bd3
Dany Marcoux <dmarcoux@posteo.de>
2020-04-17 15:45:23 +0200

    Remove unneeded networking configuration
    `useDHCP = false` is already done when enabling NetworkManager.

--- etc/etc/nixos/configuration.nix
+++ etc/etc/nixos/configuration.nix
@@ -32,15 +32,6 @@
   networking = {
     hostName = "DM-Laptop";
     networkmanager.enable = true;
-
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour
-    useDHCP = false;
-    interfaces = {
-      enp2s0.useDHCP = true;
-      wlp3s0.useDHCP = true;
-    };
   };
 
   # Select internationalisation properties
