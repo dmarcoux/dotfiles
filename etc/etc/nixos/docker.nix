49afe0be73461c5923f99455968168e0446d6964
Dany Marcoux <dmarcoux@posteo.de>
2020-04-18 17:29:54 +0200

    Install docker-compose
    While also improving comments in the file

--- etc/etc/nixos/docker.nix
+++ etc/etc/nixos/docker.nix
@@ -3,8 +3,11 @@
 { pkgs, ... }:
 
 {
-  # Enable docker
+  # Install docker and enable it on boot (it's the default)
   virtualisation.docker.enable = true;
+
+  # Install docker-compose
+  environment.systemPackages = [ pkgs.docker-compose ];
 
   # Allow user to interact with the docker daemon
   users.users.dany.extraGroups = [ "docker" ];
