f3bc2f3e10842b4f2f7e77a8fc8cf1516496e8e1
Dany Marcoux <dmarcoux@posteo.de>
2020-04-18 18:34:24 +0200

    Remove shell aliases set by NixOS' defaults

--- etc/etc/nixos/zsh.nix
+++ etc/etc/nixos/zsh.nix
@@ -6,4 +6,12 @@
   # Set zsh as default shell
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  environment = {
+    # Remove shell aliases set by NixOS' defaults
+    shellAliases = {};
+    # TODO 20.03
+    # Include ~/bin/ in $PATH
+    # homeBinInPath = true;
+  };
 }
