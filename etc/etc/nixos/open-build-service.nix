8b93e00f3b44ea533df524d68f56bc97873385c6
Dany Marcoux <dmarcoux@posteo.de>
2020-04-22 19:38:10 +0200

    Drop unneeded package cargo-vendor
    In preparation to NixOS 20.03. The subcommand `vendor` is now part of
    cargo, so this package doesn't exist anymore.

--- etc/etc/nixos/open-build-service.nix
+++ etc/etc/nixos/open-build-service.nix
@@ -5,7 +5,6 @@
 {
   environment.systemPackages = with pkgs; [
     cargo
-    cargo-vendor
     python37Packages.osc
     # TODO Package this - https://pypi.org/project/spec_cleaner/
     # spec-cleaner
