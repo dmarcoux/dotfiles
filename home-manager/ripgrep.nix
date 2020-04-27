6f27f29bb8ef563eb8de66ce4a041a6b155d864b
Dany Marcoux <dmarcoux@posteo.de>
2020-04-27 14:19:51 +0200

    Move ripgrep's config files to home-manager
    This wasn't done previously because I didn't know how.

--- home-manager/ripgrep.nix
+++ home-manager/ripgrep.nix
@@ -8,4 +8,39 @@
 
   # Set ripgrep's config path to avoid polluting the home directory
   home.sessionVariables = { RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/config"; };
+
+  # Configure ripgrep
+  xdg.configFile."ripgrep/config".text = ''
+    # Set colors
+    # Matches are blue
+    --colors=match:fg:38,139,210
+    # Line numbers are dark grey and bold
+    --colors=line:fg:88,110,117
+    --colors=line:style:bold
+    # Paths are dark grey and bold
+    --colors=path:fg:88,110,117
+    --colors=path:style:bold
+
+    # Search hidden files and directories
+    --hidden
+
+    # Searches case insensitively if the pattern is all lowercase. Search case sensitively otherwise.
+    --smart-case
+  '';
+
+  # ripgrep ignores anything matching this
+  xdg.configFile."ripgrep/.ignore".text = ''
+    .git/
+
+    *.swp
+
+    # Minified files
+    *.min.css
+    *.css.map
+    *.min.js
+    *.js.map
+
+    # Compiled files
+    .sass-cache/
+  '';
 }
