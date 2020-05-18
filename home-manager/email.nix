1bd60af11a46d5ad5bfa761ce6cab5d721d8b7bb
Dany Marcoux <dmarcoux@posteo.de>
2020-05-18 23:40:20 +0200

    Try out Astroid for my emails

--- home-manager/email.nix
+++ home-manager/email.nix
@@ -1,4 +1,5 @@
 # Setup my email accounts
+# NOTE: On the first start, run `notmuch new` to initialize the notmuch's database (it's already setup with the config below, so no need to run `notmuch setup`)
 
 { pkgs, ... }:
 
@@ -7,11 +8,23 @@
   programs.mbsync.enable = true;
   # Send emails with mstmp (it's a SMTP client)
   programs.msmtp.enable = true;
-  # Use the email client NeoMutt
-  programs.neomutt = {
+  # Index and search emails with notmuch
+  programs.notmuch = {
     enable = true;
-    # Enable and set the interval of automatic mail check
-    checkStatsInterval = 600; # seconds, so 10 minutes
+    # Fetch emails before indexing them
+    hooks.preNew = "mbsync --all";
+  };
+  # Use the email client Astroid
+  programs.astroid = {
+    enable = true;
+    # Use vim with GUI to write emails (from https://github.com/astroidmail/astroid/wiki/Customizing-editor)
+    externalEditor = "vim -g -geom 10x10 --servername %2 --socketid %3 -f -c 'set ft=mail' '+set fileencoding=utf-8' '+set ff=unix' '+set enc=utf-8' '+set fo+=w' %1";
+    extraConfig = {
+      # Enable and set the interval of automatic mail check
+      poll = { interval = 600; }; # seconds, so 10 minutes
+    };
+    # Index emails with notmuch whenever polling (which itself fetches emails with mbsync)
+    pollScript = "notmuch new";
   };
 
   accounts.email.accounts.personal = {
@@ -28,6 +41,7 @@
       create = "maildir"; # Sync folders from server
     };
     msmtp.enable = true;
-    neomutt.enable = true;
+    notmuch.enable = true;
+    astroid.enable = true;
   };
 }
