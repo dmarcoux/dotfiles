aa7be1cad655c0d04b64f0d9936026f056b4bcb2
Dany Marcoux <dmarcoux@posteo.de>
2020-05-29 14:05:04 +0200

    Enable management of user directories like the Desktop and the Music directories

--- home-manager/xdg.nix
+++ home-manager/xdg.nix
@@ -1,6 +1,27 @@
 # User configuration for XDG base directories
 
+{ pkgs, lib, ... }:
+
 {
-  # Enable management of XDG base directories
-  xdg.enable = true;
+  home.packages = [ pkgs.xdg-user-dirs ];
+
+  xdg = {
+    # Enable management of XDG base directories
+    enable = true;
+    # Enable management of user directories
+    userDirs = {
+      # Setup default user directories (Desktop, Documents, Downloads, Music, Pictures, Public, Templates and Videos)
+      enable = true;
+      # Setup extra user directories
+      extraConfig = {
+        XDG_AUDIO_DIR = "$HOME/Audio";
+        XDG_PROJETS_DIR = "$HOME/Projets";
+      };
+    };
+  };
+
+  # Generate the user directories defined above with `xdg.userDirs`
+  home.activation.xdg-user-dirs-update = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD ${pkgs.xdg-user-dirs}/bin/xdg-user-dirs-update
+  '';
 }
