0f0fa598a4e731b17c75850d3cf731e49cac7ae7
Dany Marcoux <dmarcoux@posteo.de>
2020-06-02 23:25:02 +0200

    Use unclutter-xfixes service to fix focus issue in i3

--- nixos/unclutter.nix
+++ nixos/unclutter.nix
@@ -1,7 +1,9 @@
 # System configuration for unclutter
 
 {
-  services.unclutter = {
+  # The `unclutter` service with the standard `unclutter` package causes focus to not always travel to the selected window
+# https://github.com/i3/i3/issues/3506
+  services.unclutter-xfixes = {
     # Hide mouse cursor when inactive
     enable = true;
     # Number of seconds before the cursor is marked as inactive
