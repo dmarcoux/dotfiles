61762437dbcfe22b925106f5bb676a6ffa998e3b
Dany Marcoux <dmarcoux@posteo.de>
2020-06-09 13:49:13 +0200

    Install Ruby for maintaining packages on OBS
    Also comment on why packages are required

--- nixos/open-build-service.nix
+++ nixos/open-build-service.nix
@@ -4,8 +4,11 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # For Rust packages (with cargo vendor)
     cargo
     python37Packages.osc
+    # For Ruby packages (by installing the `gem2rpm` gem)
+    ruby
     # TODO Package this - https://pypi.org/project/spec_cleaner/
     # spec-cleaner
   ];
