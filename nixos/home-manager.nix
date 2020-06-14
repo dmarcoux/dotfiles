ccf158c5a60cbfa60bc727bb7d27c166f616c245
Dany Marcoux <dmarcoux@posteo.de>
2020-06-14 16:43:31 +0200

    Clarify comment for useUserPackages option

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -8,7 +8,8 @@
   home-manager = {
     # User configuration with home-manager
     users.dany = import ../home-manager/home.nix;
-    # Whenever home-manager installs packages, install them as system packages (so what NixOS does)
+    # By default, packages will be installed to `~/.nix-profile`
+    # With useUserPackages set to `true`, packages will be installed to `/etc/profiles/per-user/$USER` instead
     useUserPackages = true;
     # Have home-manager rely on the global `pkgs` configured via the system options `nixpkgs`
     useGlobalPkgs = true;
