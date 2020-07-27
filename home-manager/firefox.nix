f3a1eb64aa37172c8ca56ef1f138d12c7e793997
Dany Marcoux <dmarcoux@posteo.de>
2020-07-27 16:23:21 +0200

    Setup firefox default profile

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -9,5 +9,20 @@
   programs.firefox = {
     # Install firefox
     enable = true;
+
+    # Setup firefox (settings are from the about:config page in firefox)
+    profiles.default.settings = {
+      "browser.aboutConfig.showWarning" = false;
+      "browser.download.dir" = "/home/dany/downloads";
+      "browser.download.folderList" = 2;
+      "browser.formfill.enable" = false;
+      "browser.search.suggest.enabled" = false;
+      "browser.startup.homepage" = "about:blank";
+      "browser.urlbar.placeholderName" = "DuckDuckGo";
+      "browser.urlbar.placeholderName.private" = "DuckDuckGo";
+      "browser.urlbar.suggest.history" = false;
+      "browser.urlbar.suggest.openpage" = false;
+      "extensions.pocket.enabled" = false;
+    };
   };
 }
