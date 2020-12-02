4edf147f68e1e30222800a9073ba257021c1e2d9
Dany Marcoux <dmarcoux@posteo.de>
2020-12-02 16:46:04 +0100

    Use solarized light theme for bat

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -4,8 +4,7 @@
   programs.bat = {
     enable = true;
     config = {
-      # TODO: Replace theme with solarized light when NixOS 20.09 is released (double-check the name of the theme)
-      theme = "ansi-light";
+      theme = "Solarized (light)";
     };
   };
 
