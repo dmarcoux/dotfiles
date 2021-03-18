dc15792c8fa0e9693bd4f49e6a64698ef6df9b49
Dany Marcoux <dmarcoux@posteo.de>
2021-03-18 22:36:53 +0100

    Configure jq with home-manager's programs.jq

--- home-manager/jq.nix
+++ home-manager/jq.nix
@@ -3,8 +3,21 @@
 { pkgs, ... }:
 
 {
-  # Install jq
-  home.packages = [ pkgs.jq ];
+  # Install and configure jq
+  programs.jq = {
+    enable = true;
+    # For details, see https://stedolan.github.io/jq/manual/#Colors
+    # TODO: Remove colors once this PR is merged: https://github.com/nix-community/home-manager/pull/1870
+    colors = {
+      null    = "1;30";
+      false   = "0;37";
+      true    = "0;37";
+      numbers = "0;37";
+      strings = "0;32";
+      arrays  = "1;37";
+      objects = "1;37";
+    };
+  };
 
   # Aliases
   programs.zsh.initExtra = ''
