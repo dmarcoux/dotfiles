ad6983c98a1b7f68a0e85da6271b076cc8ccedfb
Dany Marcoux <dmarcoux@posteo.de>
2021-03-18 22:12:08 +0100

    Configure i3status with home-manager's programs.i3status

--- home-manager/i3status.nix
+++ home-manager/i3status.nix
@@ -3,27 +3,38 @@
 { pkgs, ... }:
 
 {
-  # Install i3status
-  home.packages = [ pkgs.i3status ];
-
-  # Configure i3status
-  xdg.configFile."i3status/config".text = ''
-    general {
-      colors = true
-      interval = 1
-      output_format = 'i3bar'
-    }
-
-    order += cpu_usage
-    order += time
-
-    cpu_usage {
-      format = 'CPU:%usage'
-      separator_block_width = 20
-    }
-
-    time {
-      format = '%a %d, %B %Y %T'
-    }
-  '';
+  # Install and configure i3status
+  programs.i3status = {
+    enable = true;
+    # `man i3status` for details about the configuration options
+    general = {
+      colors = true;
+      interval = 1; # second
+      output_format = "i3bar";
+    };
+    modules = {
+      cpu_usage = {
+        position = 1; # position of this module in the i3status order
+        settings = {
+          format = "CPU:%usage";
+          separator_block_width = 20; # amount of blank pixels following the block
+        };
+      };
+      time = {
+        position = 2; # position of this module in the i3status order
+        settings = {
+          format = "%a %d, %B %Y %T";
+        };
+      };
+      # Disable default modules
+      ipv6.enable = false;
+      "wireless _first_".enable = false;
+      "ethernet _first_".enable = false;
+      "battery all".enable = false;
+      "disk /".enable = false;
+      load.enable = false;
+      memory.enable = false;
+      "tztime local".enable = false;
+    };
+  };
 }
