406b9d6a7920e24bd33f3e71152dafba3c01717e
Dany Marcoux <dmarcoux@posteo.de>
2021-07-26 22:14:17 +0200

    Fix comment indentation

--- nixos/unclutter.nix
+++ nixos/unclutter.nix
@@ -2,7 +2,7 @@
 
 {
   # The `unclutter` service with the standard `unclutter` package causes focus to not always travel to the selected window
-# https://github.com/i3/i3/issues/3506
+  # https://github.com/i3/i3/issues/3506
   services.unclutter-xfixes = {
     # Hide mouse cursor when inactive
     enable = true;
