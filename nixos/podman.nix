903ff9bfc99b8fafa60131617fd34bbf3aff33dc
Dany Marcoux <dmarcoux@posteo.de>
2021-10-16 13:28:13 +0200

    Install podman-compose

--- nixos/podman.nix
+++ nixos/podman.nix
@@ -1,6 +1,11 @@
 # System configuration for podman
 
+{ pkgs, ... }:
+
 {
   # Install podman
   virtualisation.podman.enable = true;
+
+  # Install podman-compose
+  environment.systemPackages = [ pkgs.podman-compose ];
 }
