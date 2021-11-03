0f974ffb8ab55d1a2a2cfa3e608d075cb76dd9fb
Dany Marcoux <dmarcoux@posteo.de>
2021-11-03 12:30:09 +0100

    Add TODO for Flameshot in upcoming NixOS version

--- home-manager/flameshot.nix
+++ home-manager/flameshot.nix
@@ -16,4 +16,19 @@
     drawColor=#ff0000
     drawThickness=0
   '';
+
+  # TODO: 21.11 - Use this instead of code above with manual configuration
+  # services.flameshot = {
+  #   # Start flameshot
+  #   enable = true;
+  #
+  #   # Configure flameshot
+  #   settings = {
+  #     General = {
+  #       disabledTrayIcon = false;
+  #       drawColor = "#ff0000";
+  #       drawThickness = 0;
+  #     };
+  #   };
+  # };
 }
