0971b96ac79fdb078a913075b6154c9b713b4254
Dany Marcoux <dmarcoux@posteo.de>
2021-12-13 23:26:38 +0100

    Sort scripts by path and file name

--- home-manager/scripts.nix
+++ home-manager/scripts.nix
@@ -1,11 +1,11 @@
 {
   imports = [
     ./scripts/backup-keys.nix
+    ./scripts/obs-build_vc.nix
     ./scripts/restore-backup-keys.nix
     ./scripts/rofi-pass-otp.nix
     ./scripts/rofi-pass-username.nix
     ./scripts/rofi-pass.nix
-    ./scripts/obs-build_vc.nix
     ./scripts/set-custom-keyboard-layout.nix
   ];
 }
