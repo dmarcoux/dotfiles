ea8432b80d8639b95dbf7d923f49ff967f6e9cbb
Dany Marcoux <dmarcoux@posteo.de>
2021-12-13 22:47:42 +0100

    Update home-manager's state version to 21.11

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -54,7 +54,7 @@
     homeDirectory = "/home/dany";
     # Switching to a higher state version typically requires performing some manual steps, such as data conversion or moving files
     # See release notes for state version changes: https://nix-community.github.io/home-manager/release-notes.html
-    stateVersion = "21.05";
+    stateVersion = "21.11";
     username = "dany";
   };
 }
