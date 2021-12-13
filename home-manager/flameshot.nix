2204401f3442913a291decd8f77a311d745f9715
Dany Marcoux <dmarcoux@posteo.de>
2021-12-13 23:39:41 +0100

    Configure flameshot completely with service from home-manager

--- home-manager/flameshot.nix
+++ home-manager/flameshot.nix
@@ -6,29 +6,21 @@
   # Install flameshot
   home.packages = [ pkgs.flameshot ];
 
-  # Start flameshot
-  services.flameshot.enable = true;
+  services.flameshot = {
+    # Start flameshot
+    enable = true;
 
-  # Configure flameshot
-  xdg.configFile."Dharkael/flameshot.ini".text = ''
-    [General]
-    disabledTrayIcon=false
-    drawColor=#ff0000
-    drawThickness=0
-  '';
-
-  # TODO: 21.11 - Use this instead of code above with manual configuration
-  # services.flameshot = {
-  #   # Start flameshot
-  #   enable = true;
-  #
-  #   # Configure flameshot
-  #   settings = {
-  #     General = {
-  #       disabledTrayIcon = false;
-  #       drawColor = "#ff0000";
-  #       drawThickness = 0;
-  #     };
-  #   };
-  # };
+    # Configure flameshot
+    settings = {
+      General = {
+        disabledTrayIcon = false;
+        drawColor = "#ff0000";
+        drawThickness = 1;
+        savePath = "/home/dany/pictures";
+        savePathFixed = "false";
+        setSaveAsFileExtension = "Portable Network Graphic file (PNG) (*.png)";
+        showStartupLaunchMessage = "false";
+      };
+    };
+  };
 }
