df1376333f11130102ea765414148a7c667103a2
Dany Marcoux <dmarcoux@posteo.de>
2022-01-14 15:56:10 +0100

    Add missing Steam dependencies for Stardew Valley

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,6 +1,18 @@
 # System configuration for Steam
 
+{ pkgs, ... }:
+
 {
   # Install and configure Steam
   programs.steam.enable = true;
+
+  # Add missing dependencies for certain games
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        # Unicode and Globalization support for games (Stardew Valley and perhaps other games developed with .NET)
+        icu
+      ];
+    };
+  };
 }
