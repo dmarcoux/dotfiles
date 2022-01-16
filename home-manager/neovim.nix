716ecf73ba9053d4c60454436d44becd8e83f973
Dany Marcoux <dmarcoux@posteo.de>
2022-01-16 23:15:48 +0100

    Replace coc-nvim by nvim-cmp

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -6,7 +6,56 @@
   programs.neovim = {
     enable = true;
 
+    # TODO: Convert rest of config to Lua
     extraConfig = ''
+      lua << EOF
+        -------------------- Plugins Settings
+        ---------- nvim-cmp
+        -- Display completion menu when there is one (menuone) or multiple (menu) suggestions and do not select a suggestion in the menu (noselect)
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+        local cmp = require "cmp"
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              vim.fn["vsnip#anonymous"](args.body)
+            end,
+          },
+          mapping = {
+            -- Select next suggestion with Ctrl + j
+            ["<C-j>"] = cmp.mapping.select_next_item(),
+            -- Select previous suggestion with Ctrl + k
+            ["<C-k>"] = cmp.mapping.select_prev_item(),
+            -- Trigger completion with Ctrl + Space
+            ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+            ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+            -- Exit completion
+            ["<C-e>"] = cmp.mapping({
+              i = cmp.mapping.abort(),
+              c = cmp.mapping.close(),
+            }),
+             -- Accept currently selected suggestion with Ctrl + l. With `select` to `false`, only accept explicitly selected items.
+            ["<C-l>"] = cmp.mapping.confirm({ select = false }),
+          },
+          -- Completion suggestions appear based on the order below
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' },
+          }, {
+            { name = 'buffer' },
+          })
+        })
+
+        -- TODO
+        -- Setup lspconfig.
+        -- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+        -- require("lspconfig")["<YOUR_LSP_SERVER>"].setup {
+        --   capabilities = capabilities
+        -- }
+      EOF
+
       "-------------------- Mappings
       " Mouse can be used in all modes
       set mouse=a
@@ -183,147 +232,6 @@
       "---------- wstrip-vim
       " Enable wstrip-vim on all filetypes to remove trailing whitespaces only on changed lines
       let g:wstrip_auto = 1
-
-      "---------- coc-nvim
-      " Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience.
-      set updatetime=300
-
-      " Don't pass messages to |ins-completion-menu|.
-      set shortmess+=c
-
-      " Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved.
-      set signcolumn=yes
-
-      " Use tab for trigger completion with characters ahead and navigate.
-      " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
-      " other plugin before putting this into your config.
-      inoremap <silent><expr> <TAB>
-            \ pumvisible() ? "\<C-n>" :
-            \ <SID>check_back_space() ? "\<TAB>" :
-            \ coc#refresh()
-      inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-      function! s:check_back_space() abort
-        let col = col('.') - 1
-        return !col || getline('.')[col - 1]  =~# '\s'
-      endfunction
-
-      " Use Ctrl+Space to trigger completion.
-      inoremap <silent><expr> <c-space> coc#refresh()
-
-      " If the completion menu is visible, Ctrl + j is mapped to Ctrl + n (default binding) to go to the next entry
-      " If the completion menu isn't visible, <Ctrl + j stays the same
-      inoremap <expr> <C-j> pumvisible() ? "\<C-n>" : "\<C-j>"
-      " If the completion menu is visible, Ctrl + k is mapped to Ctrl + p (default binding) to go to the previous entry
-      " If the completion menu isn't visible, <Ctrl + k stays the same
-      inoremap <expr> <C-k> pumvisible() ? "\<C-p>" : "\<C-k>"
-
-      " Use Ctrl + l to confirm completion, `<C-g>u` means break undo chain at current
-      " position. Coc only does snippet and additional edit on confirm.
-      " Enter could be remapped by other vim plugin, try `:verbose imap <C-l>`.
-      if exists('*complete_info')
-        inoremap <expr> <C-l> complete_info()["selected"] != "-1" ? "\<C-y>" : "\<C-g>u\<CR>"
-      else
-        inoremap <expr> <C-l> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
-      endif
-
-      " Use `[g` and `]g` to navigate diagnostics
-      " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-      nmap <silent> [g <Plug>(coc-diagnostic-prev)
-      nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-      " GoTo code navigation.
-      nmap <silent> gd <Plug>(coc-definition)
-      nmap <silent> gy <Plug>(coc-type-definition)
-      nmap <silent> gi <Plug>(coc-implementation)
-      nmap <silent> gr <Plug>(coc-references)
-
-      " Use K to show documentation in preview window.
-      nnoremap <silent> K :call <SID>show_documentation()<CR>
-
-      function! s:show_documentation()
-        if (index(['vim','help'], &filetype) >= 0)
-          execute 'h '.expand('<cword>')
-        else
-          call CocAction('doHover')
-        endif
-      endfunction
-
-      " Highlight the symbol and its references when holding the cursor.
-      autocmd CursorHold * silent call CocActionAsync('highlight')
-
-      " Symbol renaming.
-      nmap <leader>rn <Plug>(coc-rename)
-
-      " Formatting selected code.
-      xmap <leader>f  <Plug>(coc-format-selected)
-      nmap <leader>f  <Plug>(coc-format-selected)
-
-      augroup mygroup
-        autocmd!
-        " Setup formatexpr specified filetype(s).
-        autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
-        " Update signature help on jump placeholder.
-        autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-      augroup end
-
-      " Applying codeAction to the selected region.
-      " Example: `<leader>aap` for current paragraph
-      xmap <leader>a  <Plug>(coc-codeaction-selected)
-      nmap <leader>a  <Plug>(coc-codeaction-selected)
-
-      " Remap keys for applying codeAction to the current buffer.
-      nmap <leader>ac  <Plug>(coc-codeaction)
-      " Apply AutoFix to problem on the current line.
-      nmap <leader>qf  <Plug>(coc-fix-current)
-
-      " Map function and class text objects
-      " NOTE: Requires 'textDocument.documentSymbol' support from the language server.
-      xmap if <Plug>(coc-funcobj-i)
-      omap if <Plug>(coc-funcobj-i)
-      xmap af <Plug>(coc-funcobj-a)
-      omap af <Plug>(coc-funcobj-a)
-      xmap ic <Plug>(coc-classobj-i)
-      omap ic <Plug>(coc-classobj-i)
-      xmap ac <Plug>(coc-classobj-a)
-      omap ac <Plug>(coc-classobj-a)
-
-      " Use CTRL-S for selections ranges.
-      " Requires 'textDocument/selectionRange' support of LS, ex: coc-tsserver
-      nmap <silent> <C-s> <Plug>(coc-range-select)
-      xmap <silent> <C-s> <Plug>(coc-range-select)
-
-      " Add `:Format` command to format current buffer.
-      command! -nargs=0 Format :call CocAction('format')
-
-      " Add `:Fold` command to fold current buffer.
-      command! -nargs=? Fold :call     CocAction('fold', <f-args>)
-
-      " Add `:OR` command for organize imports of the current buffer.
-      command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
-
-      " Add (Neo)Vim's native statusline support.
-      " NOTE: Please see `:h coc-status` for integrations with external plugins that
-      " provide custom statusline: lightline.vim, vim-airline.
-      set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''')}
-
-      " Mappings for CoCList
-      " Show all diagnostics.
-      nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
-      " Manage extensions.
-      nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
-      " Show commands.
-      nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
-      " Find symbol of current document.
-      nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
-      " Search workspace symbols.
-      nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
-      " Do default action for next item.
-      nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
-      " Do default action for previous item.
-      nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
-      " Resume latest coc list.
-      nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
     '';
 
     # Available plugins: https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/vim-plugins/vim-plugin-names
@@ -352,8 +260,17 @@
       # Browse the tags of the current file and get an overview of its structure
       tagbar
 
+      # Language Server Protocol
+      nvim-lspconfig
+
+      # Snippets
+      vim-vsnip # nvim-cmp needs to have a snippets plugin
+
       # Autocompletion
-      coc-nvim
+      nvim-cmp # core plugin
+      cmp-nvim-lsp # language server client completions
+      cmp-buffer # buffer completions
+      cmp-vsnip # snippets completions (nvim-cmp needs to have a snippets plugin)
 
       # Highlight copied/yanked region
       vim-highlightedyank
