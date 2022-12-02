27564aaff2bb95d7c057ce4fe84ae3dcdda893d1
Dany Marcoux <dmarcoux@posteo.de>
2022-12-02 09:49:35 +0100

    Increase readability of home-manager/zsh/nixos.nix

--- home-manager/zsh/nixos.nix
+++ home-manager/zsh/nixos.nix
@@ -2,15 +2,20 @@
   programs.zsh.initExtra = ''
     # Delete all generations older than 14 days for all profiles
     ealias ncgd='sudo nix-collect-garbage --delete-older-than 14d'
+
     # List subscribed channels
     ealias ncl='sudo nix-channel --list'
+
     # Update and activate channels
     ealias ncu='sudo nix-channel --update'
+
     # Build, activate and update boot default
     ealias nrs='sudo nixos-rebuild switch'
+
     # Search for a package
     ealias ns='nix search'
     ealias nsu='nix search --update-cache'
+
     # Start a pure nix-shell
     ealias nsp='nix-shell --pure'
   '';
