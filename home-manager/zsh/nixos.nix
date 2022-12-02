23e07deee4b97cd091fffc59d3b724bd7d9f8ea5
Dany Marcoux <dmarcoux@posteo.de>
2022-12-02 10:03:52 +0100

    Add function and alias to show differences between NixOS generations

--- home-manager/zsh/nixos.nix
+++ home-manager/zsh/nixos.nix
@@ -18,5 +18,19 @@
 
     # Start a pure nix-shell
     ealias nsp='nix-shell --pure'
+
+    # Show differences between two NixOS generations
+    nixos_generation_diffs(){
+      if [ -z "$2" ]; then
+        # Display usage if less than 2 parameters were given
+        echo "Usage: ''${funcstack[1]} OLDER_GENERATION_NUMBER YOUNGER_GENERATION_NUMBER"
+        return
+      fi
+
+      nix store diff-closures "/nix/var/nix/profiles/system-$1-link" "/nix/var/nix/profiles/system-$2-link" | less
+    }
+
+    # Show incremental differences between all NixOS generations on my system
+    ealias npdc='nix profile diff-closures --profile /nix/var/nix/profiles/system | less'
   '';
 }
