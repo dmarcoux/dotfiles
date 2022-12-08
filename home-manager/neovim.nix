3d653a480008a7cea12e1907c85488e3a5e9eff0
Dany Marcoux <dmarcoux@posteo.de>
2022-12-08 21:55:46 +0100

    Add comment note on how to disable wstrip in a buffer

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -229,6 +229,7 @@
 
       "---------- wstrip-vim
       " Enable wstrip-vim on all filetypes to remove trailing whitespaces only on changed lines
+      " It's useful in rare occasions to disable this in a buffer, do it with the Neovim command `:let b:wstrip_auto = 0`
       let g:wstrip_auto = 1
 
       "---------- vim-rooter
