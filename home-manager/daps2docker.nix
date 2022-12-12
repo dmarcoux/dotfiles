a3516516909fb680c5c56a9b926199e07fef5d91
Dany Marcoux <dmarcoux@posteo.de>
2022-12-12 14:36:18 +0100

    Remove outdated FIXME comment
    This has been fixed since I've written that comment. Yay!

--- home-manager/daps2docker.nix
+++ home-manager/daps2docker.nix
@@ -3,8 +3,6 @@
 { config, ... }:
 
 {
-   # FIXME: Somehow, even with mkOutOfStoreSymlink, it does create a symlink, but if the source file changes, it's not reflected in `bin/daps2docker`...
-   #        The fix is to rebuild.
    # The symlink is going to be broken if https://github.com/opensuse/daps2docker isn't cloned under /home/dany/projets/
    home.file."bin/daps2docker".source = config.lib.file.mkOutOfStoreSymlink "/home/dany/projets/daps2docker/daps2docker.sh";
 }
