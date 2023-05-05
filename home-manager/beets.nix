4b577cfa400dd25adf305d3b6514658a672a49c6
Dany Marcoux <dmarcoux@posteo.de>
2023-05-05 20:44:33 +0200

    Set path formats and directory for beets

--- home-manager/beets.nix
+++ home-manager/beets.nix
@@ -6,6 +6,8 @@
     # Even settings with their default value are listed below to understand what beets does
     # and if those values were to ever change, beets would still act the same way
     settings = {
+      directory = "~/music";
+
       import = { # When using `beet import`...
         # Write metadata tags to files
         write = true;
@@ -20,6 +22,13 @@
 
         # Whenever possible, ask if an import should be resumed
         resume = "ask";
+      };
+
+      # Path formats - Define how beets store files. For details, see https://beets.readthedocs.io/en/v1.6.0/reference/pathformat.html#available-values
+      paths = {
+        default = "$format/$albumartist/$album%aunique{}/$track $title"; # Default whenever another path format doesn't match
+        singleton = "$format/Non-Album/$artist/$title"; # Single tracks
+        comp = "$format/Compilations/$album%aunique{}/$track $title"; # Compilations
       };
     };
   };
