f642b7816458c9666e6061c2c190845ea51fb149
Dany Marcoux <dmarcoux@posteo.de>
2023-05-08 20:09:33 +0200

    Uninstall DeaDBeeF

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -150,7 +150,6 @@
       assign [class="^thunderbird$"] → $WS4
 
       assign [class="^Audacious$"] → $WS5
-      assign [class="^Deadbeef$"] → $WS5
       # "assign" doesn't work for the Spotify client since it's not correctly setting the hints when mapping the window
       # https://github.com/i3/i3/issues/2060
       for_window [class="^Spotify$"] move container to workspace $WS5
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,6 @@ in
     ../secrets/certificates/work.nix
     ../secrets/vpn/work.nix
     ./chromium.nix
-    ./deadbeef.nix
     ./evince.nix
     ./i3.nix
     ./keyboard.nix
