3f5adb9a35451e3a85760ba1553a79e93edd4d2f
Dany Marcoux <dmarcoux@posteo.de>
2023-06-20 22:21:06 +0200

    Use bar for cursor shape in insert mode

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -15,8 +15,8 @@
         # Highlight all lines and columns with a cursor (there can be multiple cursors in Helix)
         cursorline = true;
         cursorcolumn = true;
-        # Set cursor shape to a bar in normal mode (the default is a block)
-        cursor-shape.normal = "bar";
+        # Set cursor shape to a bar in insert mode (the default is a block)
+        cursor-shape.insert = "bar";
         # Display LSP progress messages below statusline
         lsp.display-messages = true;
         # Number of lines of padding around the edge of the screen when scrolling
