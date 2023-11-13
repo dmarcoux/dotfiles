9ea4cf5b01afc7dfd45c34bc5aa30b8558ea19e8
Dany Marcoux <dmarcoux@posteo.de>
2023-11-13 21:37:30 +0100

    Add desktop host

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,31 @@
+# Host-specific configuration for my laptop at work
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix # Include the results of the hardware scan
+    ../../nixos/configuration.nix # System configuration
+    # Secrets for work
+    ../../secrets/certificates/work.nix
+    ../../secrets/vpn/work.nix
+    ../../secrets/ssh/work.nix
+
+  ];
+
+  # Use the systemd-boot EFI boot loader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "DM-Desktop";
+    networkmanager.enable = true;
+  };
+
+  # Enable bluetooth support
+  hardware.bluetooth.enable = true;
+  # Install blueman, a GUI to manage Bluetooth devices
+  services.blueman.enable = true;
+}
