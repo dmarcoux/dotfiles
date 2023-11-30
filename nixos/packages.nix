9d92ccc76cd5fc414e03344e01c345d4f7767454
Dany Marcoux <git@dmarcoux.com>
2023-11-30 15:58:50 +0100

    Uninstall android-file-transfer
    It's unreliable, working about 20% of the time. I need to find a more
    stable alternative.

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -6,8 +6,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Reliable MTP client with minimalistic UI to transfer files from Android phones and other devices supporting the Media Transfer Protocol
-    android-file-transfer
     # Deployment tool for work
     ansible
     # Frontend to XRandR to manage multiple monitors
