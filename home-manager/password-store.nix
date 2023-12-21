aa2584185e3a748c61ff11b3d44ffb6cff29a620
Dany Marcoux <git@dmarcoux.com>
2023-12-21 16:08:04 +0100

    Turn new_password function into its own script

--- home-manager/password-store.nix
+++ home-manager/password-store.nix
@@ -17,32 +17,41 @@
   # Install xdotool to type passwords
   home.packages = [pkgs.xdotool];
 
+  # TODO: Make this script a password-store extension, see https://www.passwordstore.org/#extensions
+  home.file."bin/new_password" = {
+    executable = true;
+    text = ''
+      #!${pkgs.bash}/bin/bash
+      # Create password in the password store
+
+      # Exit immediately if a command exits with a non-zero status, like when interrupted with Ctrl+C
+      set -e
+
+      DIRECTORY=$(gum choose --header="Select a directory for the password" "me" "work")
+
+      # Enforce the presence of an identifier by looping until it isn't an empty string
+      while [[ -z "$IDENTIFIER" ]]; do
+        IDENTIFIER=$(gum input --header="Write the identifier for the password" --placeholder="example.com")
+      done
+
+      ACCOUNT=$(gum input --header="Write the account associated to the password" --placeholder="Username/Email/Login/etc...")
+
+      PASSWORD_FILE="$DIRECTORY/$IDENTIFIER"
+
+      pass insert "$PASSWORD_FILE"
+
+      # If no account was provided, we're done
+      if [[ -n "$ACCOUNT" ]]; then
+        PASSWORD=$(pass show "$PASSWORD_FILE")
+
+        pass insert --multiline --force "$PASSWORD_FILE" <<< $(echo -e "$PASSWORD\nUsername: $ACCOUNT")
+      fi
+    '';
+  };
+
+
   # Helper functions for common tasks
   programs.zsh.initExtra = ''
-    # Setup new password
-    new_password(){
-      if [ -z "$1" ]; then
-        # Display usage if no parameters given
-        echo "Usage: ''${funcstack[1]} me/some_website.com=account (account is the email/username in the login)"
-        return
-      fi
-
-      password_file="$1"
-      password_file_without_username=$(echo "$password_file" | sed -e "s|^\(.*\)=.*|\1|g")
-      pass insert "$password_file_without_username"
-
-      # If no username was provided, we're done
-      if [ "$password_file" = "$password_file_without_username" ]; then
-        return
-      fi
-
-      # A username was provided, so we need to overwrite the existing file to add the username
-      username=$(echo "$password_file" | sed -e "s|^.*=\(.*\)|\1|g");
-      password=$(pass "$password_file_without_username");
-
-      pass insert --multiline --force "$password_file_without_username" <<< $(echo -e "$password\nUsername: $username")
-    }
-
     generate_password(){
        if [ -z "$1" ]; then
         # Display usage if no parameters given
