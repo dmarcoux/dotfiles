0d0715b4e66e59da9a29d3efb2f309a883028974
Dany Marcoux <git@dmarcoux.com>
2024-04-19 22:06:57 +0200

    Do not pollute $HOME with IEx configuration file

--- home-manager/elixir.nix
+++ home-manager/elixir.nix
@@ -1,7 +1,11 @@
 # User configuration for Elixir
 
+{ config, ... }:
+
 {
   home.sessionVariables = {
+    # Location of the global IEx configuration file (.iex.exs)
+    IEX_HOME = "${config.xdg.configHome}/elixir";
     # In IEx, use Neovim whenever opening files with `open` (see `h open` for details)
     ELIXIR_EDITOR = "nvim +__LINE__ __FILE__";
     # Persist history of the IEx (Elixir) and erl (Erlang) shells
@@ -9,7 +13,7 @@
   };
 
   # Configure IEx - https://hexdocs.pm/iex/IEx.html#configure/1
-  home.file.".iex.exs".text = ''
+  xdg.configFile."elixir/.iex.exs".text = ''
     # Always print charlists as lists, so `[ 97, 98, 99 ]` is printed as `[ 97, 98, 99 ]`, not as `'abc'`
     IEx.configure(inspect: [ charlists: :as_lists ])
   '';
