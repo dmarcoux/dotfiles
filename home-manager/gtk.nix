857c1180076a84f66017c959bb2cf11708a21651
Dany Marcoux <git@dmarcoux.com>
2024-04-20 10:57:44 +0200

    Do not pollute $HOME with GTK config files

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,9 +1,14 @@
 # User configuration for GTK
 
+{ config, ... }:
+
 {
   gtk = {
     # Enable GTK 2/3 configuration
     enable = true;
+
+    # Do not pollute $HOME with config files
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
 
     # Set bookmarks in the sidebar of the GTK file browser (used in Firefox, Thunderbird, etc...)
     gtk3.bookmarks = [
