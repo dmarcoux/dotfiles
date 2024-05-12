c7bb42a326510bbf4ea11db0e7b4e771b55c3f8d
Dany Marcoux <git@dmarcoux.com>
2024-05-12 20:42:19 +0200

    Add -X option to curljson alias

--- home-manager/zsh/curl.nix
+++ home-manager/zsh/curl.nix
@@ -1,6 +1,6 @@
 {
   programs.zsh.initExtra = ''
     # --data '{"abc":"123","def":"'my text'"}' or --data @filename (for some file containing JSON)
-    ealias curljson='curl --verbose --header "Content-Type: application/json" --data' # See above for --data, then add  http://whatever.com
+    ealias curljson='curl --verbose --header "Content-Type: application/json" -X POST --data' # See above for --data, then add  http://whatever.com
   '';
 }
