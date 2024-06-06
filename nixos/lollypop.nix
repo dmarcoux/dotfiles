8b84a8a77c9e6ca96c98fd1a7a2352b65d73e7ef
Dany Marcoux <git@dmarcoux.com>
2024-06-06 14:52:50 +0200

    Remove unneeded configuration for lollypop when running in GNOME
    This isn't needed anymore now that I'm using GNOME. dconf and GNOME
    keyring both run by default.

--- nixos/lollypop.nix
+++ nixos/lollypop.nix
@@ -5,10 +5,4 @@
 {
   # Install lollypop
   environment.systemPackages = [ pkgs.lollypop ];
-
-  # dconf is needed to be able to store changes in lollypop's settings
-  programs.dconf.enable = true;
-
-  # Gnome keyring is needed to store credentials for the various web services (last.fm, etc...) integrated into lollypop
-  services.gnome.gnome-keyring.enable = true;
 }
