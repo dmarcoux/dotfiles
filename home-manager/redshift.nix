a635e869950179e11e10ab834633579cc83b7cf8
Dany Marcoux <git@dmarcoux.com>
2024-07-02 09:35:04 +0200

    Use manual provider for redshift while GeoClue2 isn't working

--- home-manager/redshift.nix
+++ home-manager/redshift.nix
@@ -10,8 +10,13 @@
   services.redshift = {
     enable = true;
 
+    # TODO: Switch back to GeoClue2 once this issue is addressed https://github.com/NixOS/nixpkgs/issues/321121
     # Use GeoClue2 as location provider
-    provider = "geoclue2";
+    # provider = "geoclue2";
+    provider = "manual";
+    # Freiburg im Breisgau
+    latitude = 47.9990;
+    longitude = 7.8421;
 
     # Colour temperature to use at day/night, between 1000 and 25000 K
     temperature = {
