50424371d52429928c777ef159d5c76d4a30cafb
Dany Marcoux <git@dmarcoux.com>
2024-07-24 20:53:47 +0200

    Correctly unlock GNOME Keyring
    The previous commit 0a4f1e5 didn't fix the issue. Now, this works!

--- nixos/gnome-keyring.nix
+++ nixos/gnome-keyring.nix
@@ -1,6 +1,8 @@
 # System configuration for GNOME Keyring
 # It cannot be done in home-manager: https://github.com/nix-community/home-manager/issues/1454
 
+{ pkgs, lib, ... }:
+
 {
   # Launch GNOME Keyring to store security credentials. It's used by many applications like JetBrains IDEs to store database passwords
   services.gnome.gnome-keyring.enable = true;
@@ -8,6 +10,10 @@
   # Install Seahorse to view/manage encryption keys and passwords in the GNOME Keyring
   programs.seahorse.enable = true;
 
-  # For GNOME Keyring to unlock by default
+  # For GNOME Keyring to unlock by default, but this doesn't work since the X session is managed by home-manager (which creates ~/.xsession)...
   services.xserver.updateDbusEnvironment = true;
+  # ... so in this case, we need to run the command below as noted in https://github.com/NixOS/nixpkgs/issues/174099#issuecomment-1201697954
+  services.xserver.displayManager.sessionCommands = ''
+    ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
+  '';
 }
