e63772cdee4cd8cc3e0baac1f62eea4052d77901
Dany Marcoux <git@dmarcoux.com>
2024-07-25 11:12:03 +0200

    Use the 1Password SSH agent for all hosts

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -6,5 +6,11 @@
 
     # Keep SSH sessions alive by sending, every minute, a keep-alive signal to hosts
     serverAliveInterval = 60;
+
+    # Use the 1Password SSH agent for all hosts
+    extraConfig = ''
+      Host *
+          IdentityAgent ~/.1password/agent.sock
+    '';
   };
 }
