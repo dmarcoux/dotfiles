d8b76065484ff3b4672efd2c98b33b2ef9b20dd1
Dany Marcoux <git@dmarcoux.com>
2024-07-27 09:57:24 +0200

    Sign git commits with SSH

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,7 +1,11 @@
 # User configuration for git
 
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 
+let
+  # My public SSH key
+  sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUwJdHmZu1Cz8kLQTGtKxpzneRJw3p0wskhRi+wUtRx3of3Hakcceiw8n6N6UJQ7vpe9MoJv/e9IkdZyp5/QvHEdI/ZrrLkWc+WzFuC1nDMuu0yiV7TLEn1E63EQDgZCrsPgqOElYm51+zL4sL3ISMF7yedi2zdZNAI/kBVWaurxy2Szkw/0c+KfT8N+YO1FKJLTo9KP8hs2I7x7SUrIsPMLDYSvS//i4O9/HCZgj43StYDxfgAIgWAeD1Q5gHrjlFdmueESFeXGBDNLJwJE8LUrMUqgg9+smwt3nH/NW+klQmWzy0y9dpb5sbfHSz2PnoE3aCTNCAaFQe14oE+cN2xPyvG0Uueeg3fIFJvTZMGJB9G+g1IG2PeYo6UvarxcfUDiwEYhw9gZG9nS7+gZGnbcuezIeq7xzrW5jLnCaCI/R7vHK3yN0/8VerX7VU3CXcmMGldsaKd0QxDk1aArVqLNR+tgYpAj2MIFOprYz0oNUOKrqSlfmyVQOIbeqcnTa+IhV7uP+hjuwO6HFbkn3im6n7BGVX7aTTe/RGQBbbR9qad6Of3bRJJe0ukLwti3R4Vtx8RJ1vZ5v/k4rTHM24AzlXd6QtP3KHZ1PPJcT9nFQtQ8gcCPrP2ACXo6p9KGH5caIBAy8dDWFWgwtsvQbDsbogOOQLh4Gbtm5ymIgOcw==";
+in
 {
   # Install extra packages
   home.packages = with pkgs; [
@@ -52,6 +56,20 @@
         email = "git@dmarcoux.com";
         name = "Dany Marcoux";
       };
+      # Sign commits with SSH. 1Password stores the SSH key used in signing commits
+      # https://developer.1password.com/docs/ssh/git-commit-signing
+      commit = { gpgsign = true; }; # To avoid including the `-S` flag with each commit
+      gpg = {
+        format = "ssh";
+        ssh = {
+          # To avoid having to set SSH_AUTH_SOCK myself, this is handled by 1Password GUI
+          program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+          # To be able to verify commit signatures locally with `git log --show-signature`
+          allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+        };
+      };
+      # Use my public SSH key to sign git commits
+      user.signingKey = "${sshKey}";
     };
 
     # List of paths that should be globally ignored
@@ -70,13 +88,10 @@
 
     # Install and setup git-lfs
     lfs.enable = true;
-
-    # Sign commits using GnuPG
-    signing = {
-      key = "82FA5A4A";
-      signByDefault = true;
-    };
   };
+
+  # Allow signing git commits with my SSH key
+  home.file.".ssh/allowed_signers".text = "* ${sshKey}";
 
   # Install and configure GitHub CLI (https://cli.github.com/)
   programs.gh = {
