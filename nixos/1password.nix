97790efe7ca6f2a53100d1132f85410cef0fe550
Dany Marcoux <git@dmarcoux.com>
2024-09-24 21:49:09 +0200

    Install 1password packages from nixos-unstable input

--- nixos/1password.nix
+++ nixos/1password.nix
@@ -1,13 +1,22 @@
 # System configuration for 1Password
 # Documentation at https://nixos.wiki/wiki/1Password
 
+{ pkgs-unstable, ... }:
+
 {
-  # Install 1Password CLI
-  programs._1password.enable = true;
+  programs._1password = {
+    # Install 1Password CLI
+    enable = true;
+    # Always get the latest version available
+    package = pkgs-unstable._1password;
+  };
 
   # Install and configure 1Password GUI
   programs._1password-gui = {
     enable = true;
+    # Always get the latest version available
+    package = pkgs-unstable._1password-gui;
+
     # Certain features, including CLI integration and system authentication support, require enabling PolKit integration
     polkitPolicyOwners = [ "dany" ];
   };
