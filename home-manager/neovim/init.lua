5c006c4951498b2b5fcc18c224a7429e0fc66079
Dany Marcoux <git@dmarcoux.com>
2024-10-04 20:05:18 +0200

    Move Neovim mappings at the top of the configuration
    It's recommended to set the leader key before plugins are loaded,
    otherwise the default (thus wrong) leader key will be used.

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -1,3 +1,43 @@
+-------------------- Mappings
+---- Mappings with vim.keymap.set are non-recursive by default
+-- Mouse can be used in all modes
+vim.opt.mouse = 'a'
+
+-- Set leader key to define extra key combinations with this. For example, to save the current file, define: map <leader>w
+-- It's recommended to set the leader key before plugins are loaded, otherwise the default (thus wrong) leader key will be used
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- Allow Backspace, Space, left arrow, right arrow, h and l keys to move to the previous/next line when the cursor is on the first/last character in the line
+vim.opt.whichwrap = 'b,s,<,>,h,l'
+
+---------- Insert Mode
+-- Non-recursive mapping for Shift+Tab to unindent
+vim.keymap.set('i', '<S-Tab>', '<C-D>')
+
+---------- Normal Mode
+-- Non-recursive mappings for Ctrl + J and Ctrl + K to move the current line(s) up / down
+vim.keymap.set('n', '<C-j>', ':m .+1<CR>==')
+vim.keymap.set('n', '<C-k>', ':m .-2<CR>==')
+
+---------- Visual and Select Modes
+-- Non-recursive mappings for Ctrl + J and Ctrl + K to move selected line(s) up / down
+vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
+
+---------- Normal, Visual and Operator Pending Modes
+-- Non-recursive mappings for n to always search forward and N to always search backward (for / and ?)
+--https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+vim.keymap.set({'n', 'v', 'o'}, 'n', "'Nn'[v:searchforward]", { expr = true })
+vim.keymap.set({'n', 'v', 'o'}, 'N', "'nN'[v:searchforward]", { expr = true })
+
+-- Non-recursive mappings to move around long wrapped lines
+vim.keymap.set({'n', 'v', 'o'}, 'j', 'gj')
+vim.keymap.set({'n', 'v', 'o'}, 'k', 'gk')
+
+-- Non-recursive mapping for leader + Enter to disable text highlighting
+vim.keymap.set({'n', 'v', 'o'}, '<leader><CR>', ':noh<CR>', { silent = true })
+
 -------------------- Plugins Settings
 ---------- nvim-cmp
 -- Display completion menu when there is one (menuone) or multiple (menu) suggestions and do not select a suggestion in the menu (noselect)
@@ -56,45 +96,6 @@ require("lspconfig")["gopls"].setup({
   cmd = { "/nix/store/6nysag23irns7ldbsdrjwc1ckap1hqm6-gopls-0.16.2/bin/gopls" },
   capabilities = capabilities
 })
-
--------------------- Mappings
----- Mappings with vim.keymap.set are non-recursive by default
--- Mouse can be used in all modes
-vim.opt.mouse = 'a'
-
--- Set leader key to define extra key combinations with this. For example, to save the current file, define: map <leader>w
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
-
--- Allow Backspace, Space, left arrow, right arrow, h and l keys to move to the previous/next line when the cursor is on the first/last character in the line
-vim.opt.whichwrap = 'b,s,<,>,h,l'
-
----------- Insert Mode
--- Non-recursive mapping for Shift+Tab to unindent
-vim.keymap.set('i', '<S-Tab>', '<C-D>')
-
----------- Normal Mode
--- Non-recursive mappings for Ctrl + J and Ctrl + K to move the current line(s) up / down
-vim.keymap.set('n', '<C-j>', ':m .+1<CR>==')
-vim.keymap.set('n', '<C-k>', ':m .-2<CR>==')
-
----------- Visual and Select Modes
--- Non-recursive mappings for Ctrl + J and Ctrl + K to move selected line(s) up / down
-vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
-
----------- Normal, Visual and Operator Pending Modes
--- Non-recursive mappings for n to always search forward and N to always search backward (for / and ?)
---https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-vim.keymap.set({'n', 'v', 'o'}, 'n', "'Nn'[v:searchforward]", { expr = true })
-vim.keymap.set({'n', 'v', 'o'}, 'N', "'nN'[v:searchforward]", { expr = true })
-
--- Non-recursive mappings to move around long wrapped lines
-vim.keymap.set({'n', 'v', 'o'}, 'j', 'gj')
-vim.keymap.set({'n', 'v', 'o'}, 'k', 'gk')
-
--- Non-recursive mapping for leader + Enter to disable text highlighting
-vim.keymap.set({'n', 'v', 'o'}, '<leader><CR>', ':noh<CR>', { silent = true })
 
 -------------------- Command Mode
 -- Non-recursive abbreviations for common typos when saving/quiting
