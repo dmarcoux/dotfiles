d769568c761354a7c96eec23b394d9992372b359
Dany Marcoux <git@dmarcoux.com>
2024-10-04 16:44:26 +0200

    Start migration of Neovim mappings to Lua

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -57,32 +57,33 @@ require("lspconfig")["gopls"].setup({
   capabilities = capabilities
 })
 
+-------------------- Mappings
+-- Mouse can be used in all modes
+vim.opt.mouse = 'a'
+
+-- Set leader key to define extra key combinations with this. For example, to save the current file, define: map <leader>w
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- Allow Backspace, Space, left arrow, right arrow, h and l keys to move to the previous/next line when the cursor is on the first/last character in the line
+vim.opt.whichwrap = 'b,s,<,>,h,l'
+
+---------- Insert Mode
+-- Non-recursive mapping for Shift+Tab to unindent
+vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-D>', { noremap = true })
+
+---------- Normal Mode
+-- Non-recursive mappings for Ctrl + J and Ctrl + K to move the current line(s) up / down
+vim.api.nvim_set_keymap('n', '<C-j>', ':m .+1<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':m .-2<CR>==', { noremap = true })
+
+---------- Visual and Select Modes
+-- Non-recursive mappings for Ctrl + J and Ctrl + K to move selected line(s) up / down
+vim.api.nvim_set_keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", { noremap = true })
+
 vim.cmd [[
 "-------------------- Mappings
-" Mouse can be used in all modes
-set mouse=a
-
-" Define extra key combinations with a map leader. For example, to save the current file, define: map <leader>w
-let mapleader=","
-
-" Left and right movements will go to the previous or next line when at start or end of line
-" https://vim.fandom.com/wiki/Automatically_wrap_left_and_right
-set whichwrap+=<,>,h,l
-
-"---------- Insert Mode
-" Non-recursive mapping for Shift+Tab to unindent
-inoremap <S-Tab> <C-D>
-
-"---------- Normal Mode
-" Non-recursive mappings for Ctrl + J and Ctrl + K to move the current line(s) up / down
-nnoremap <C-j> :m .+1<CR>==
-nnoremap <C-k> :m .-2<CR>==
-
-"---------- Visual and Select Modes
-" Non-recursive mappings for Ctrl + J and Ctrl + K to move selected line(s) up / down
-vnoremap <C-j> :m '>+1<CR>gv=gv
-vnoremap <C-k> :m '<-2<CR>gv=gv
-
 "---------- Normal, Visual and Operator Pending Modes
 " Non-recursive mappings for n to always search forward and N to always search backward (for / and ?)
 noremap <expr> n  'Nn'[v:searchforward]
