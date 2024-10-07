b79aa3b459c091664868a16ebf8e3fa9030c3a1b
Dany Marcoux <git@dmarcoux.com>
2024-10-07 10:13:11 +0200

    Move Neovim autocommands to their own file

---
+++ home-manager/neovim/autocommands.nix
@@ -0,0 +1,50 @@
+{
+  # Autocommands which aren't directly related to a Neovim plugin
+  # See `:help lua-guide-autocommands` for details
+  programs.nixvim.autoCmd = [
+    {
+      desc = "Set options in Markdown files";
+      event = [ "BufNewFile" "BufReadPost" ];
+      pattern = "*.md";
+      callback = {
+        __raw = ''
+          function()
+            -- Force Markdown type for all files with the .md extension (from tpope/vim-markdown)
+            vim.opt.filetype = 'markdown'
+            -- Wrap lines at 80 characters
+            vim.opt_local.textwidth = 80
+          end
+        '';
+      };
+    }
+    {
+      desc = "When the quickfix window opens, move it the very bottom";
+      event = "FileType"; # `qf` refers to the quickfix window (Search for `qf` in `:help filetypes`)
+      pattern = "qf";
+      callback = {
+        __raw = ''
+          function()
+            vim.cmd('wincmd J') -- See `:help :wincmd` and `:help ^WJ`
+          end
+        '';
+      };
+    }
+    {
+      desc = "Highlight when yanking (copying) text";
+      event = "TextYankPost";
+      callback = {
+        __raw = ''
+          function()
+            -- See `:help vim.highlight.on_yank()`
+            vim.highlight.on_yank({ timeout = 1000 }) -- highlight for 1 second
+
+            -- Force Markdown type for all files with the .md extension (from tpope/vim-markdown)
+            vim.opt.filetype = 'markdown'
+            -- Wrap lines at 80 characters
+            vim.opt_local.textwidth = 80
+          end
+        '';
+      };
+    }
+  ];
+}
