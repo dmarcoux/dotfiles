209b5dc5ed4bb97a1f9203347670a621d46ec218
Dany Marcoux <git@dmarcoux.com>
2024-10-30 16:29:55 +0100

    Install latest version of Docker

--- nixos/docker.nix
+++ nixos/docker.nix
@@ -7,7 +7,7 @@
     # Enable docker on boot
     enable = true;
     # Install docker (from unstable to get the latest version)
-    package = pkgs-unstable.docker_26;
+    package = pkgs-unstable.docker;
   };
 
   # Install docker-compose
