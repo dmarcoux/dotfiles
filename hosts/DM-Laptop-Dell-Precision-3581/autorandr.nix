2bfc9c8acb1f1987da305a80152a46adcc7b047e
Dany Marcoux <git@dmarcoux.com>
2025-01-07 11:14:46 +0100

    Setup autorandr for the office

--- hosts/DM-Laptop-Dell-Precision-3581/autorandr.nix
+++ hosts/DM-Laptop-Dell-Precision-3581/autorandr.nix
@@ -4,7 +4,7 @@
   # Find available displays with xrandr
   home-manager.users.dany.programs.autorandr = {
     profiles = {
-      both = {
+      laptop-and-hdmi = {
         config = {
           "HDMI-1" = {
             enable = true;
@@ -30,7 +30,7 @@
           "eDP-1" = "*";
         };
       };
-      external = {
+      hdmi = {
         config = {
           "HDMI-1" = {
             enable = true;
@@ -47,6 +47,70 @@
         };
         fingerprint = {
           "HDMI-1" = "*";
+        };
+      };
+      docking-external-laptop-external = {
+        config = {
+          "DP-2" = {
+            enable = true;
+            mode = "1920x1080";
+            position = "0x0";
+            primary = true;
+            rate = "60.00";
+            crtc = 0;
+          };
+          "eDP-1" = {
+            enable = true;
+            mode = "1920x1080";
+            position = "1920x0";
+            primary = true;
+            rate = "60.00";
+            crtc = 0;
+          };
+          "DP-1-2" = {
+            enable = true;
+            mode = "1920x1080";
+            position = "3840x0";
+            primary = true;
+            rate = "60.00";
+            crtc = 0;
+          };
+          "DP-1".enable = false;
+          "HDMI-1".enable = false;
+          "HDMI-2".enable = false;
+        };
+        fingerprint = {
+          "DP-2" = "*";
+          "eDP-1" = "*";
+          "DP-1-2" = "*";
+        };
+      };
+      docking-only-externals = {
+        config = {
+          "DP-2" = {
+            enable = true;
+            mode = "1920x1080";
+            position = "0x0";
+            primary = true;
+            rate = "60.00";
+            crtc = 0;
+          };
+          "DP-1-2" = {
+            enable = true;
+            mode = "1920x1080";
+            position = "3840x0";
+            primary = true;
+            rate = "60.00";
+            crtc = 0;
+          };
+          "eDP-1".enable = false;
+          "DP-1".enable = false;
+          "HDMI-1".enable = false;
+          "HDMI-2".enable = false;
+        };
+        fingerprint = {
+          "DP-2" = "*";
+          "DP-1-2" = "*";
         };
       };
       default = { # Laptop screen only
