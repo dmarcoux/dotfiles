ad566d2f3311c0c813e6bda35d714290359bcb1f
Dany Marcoux <git@dmarcoux.com>
2025-01-25 16:17:19 +0100

    Setup multiple keyboard layouts for pair-programming

--- nixos/keyboards.nix
+++ nixos/keyboards.nix
@@ -3,6 +3,17 @@
 { pkgs, ... }:
 
 {
+  services.xserver.xkb = {
+    # Set multiple keyboard layouts (This is useful when pair-programming with colleagues which are used to another layout)
+    # - Canadian Multilingual: https://kbdlayout.info/kbdcan
+    # - German: https://kbdlayout.info/kbdgr
+    # - US: https://kbdlayout.info/kbdus
+    layout = "ca(multix),de,us";
+    # AltGr + Space produces a normal space instead of a non-breakable space
+    # Alt + Space switches to the next keyboard layout
+    options = "nbsp:none,grp:alt_space_toggle";
+  };
+
   # Enable udev rules for ZSA keyboards. This is needed to be able to flash a new configuration on the keyboards
   hardware.keyboard.zsa.enable = true;
 
